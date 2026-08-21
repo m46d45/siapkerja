9d6bd3ccec20d4424d2f16f94137b2b65222b502
m46d45 <muhabduh@gmail.com>
2026-08-21 11:47:30 +0700

    feat: add data modules, state, vercel config

---
+++ js/data/teams.js
@@ -0,0 +1,61 @@
+// SiapKerja! - Tim Kerja Data
+// 6 Tim Kerja dengan warna identitas untuk color-coding Sticky Notes & Gantt
+
+export const teams = [
+  {
+    id: "pondasi-site",
+    name: "Tim Pondasi & Site",
+    shortName: "Pondasi",
+    color: "#A0522D",
+    colorName: "Coklat",
+    capacity: 4,
+    assigned: 0
+  },
+  {
+    id: "struktur",
+    name: "Tim Struktur",
+    shortName: "Struktur",
+    color: "#1E40AF",
+    colorName: "Biru Tua",
+    capacity: 5,
+    assigned: 0
+  },
+  {
+    id: "dinding",
+    name: "Tim Dinding / Masonry",
+    shortName: "Dinding",
+    color: "#EA580C",
+    colorName: "Oranye",
+    capacity: 6,
+    assigned: 0
+  },
+  {
+    id: "atap-kayu",
+    name: "Tim Atap & Kayu",
+    shortName: "Atap",
+    color: "#15803D",
+    colorName: "Hijau",
+    capacity: 4,
+    assigned: 0
+  },
+  {
+    id: "mep",
+    name: "Tim MEP",
+    shortName: "MEP",
+    color: "#7C3AED",
+    colorName: "Ungu",
+    capacity: 3,
+    assigned: 0
+  },
+  {
+    id: "finishing",
+    name: "Tim Finishing",
+    shortName: "Finishing",
+    color: "#CA8A04",
+    colorName: "Amber",
+    capacity: 5,
+    assigned: 0
+  }
+];
+
+export default teams;
